69d4f23dc1680acf321a0f91384a4ceedde1eae4
Venkie07 <sit24ad018@sairamtap.edu.in>
2026-01-17 11:07:22 +0530

    Update A2.py

--- A2.py
+++ A2.py
@@ -7,39 +7,40 @@ from datetime import datetime
 app = Flask(__name__)
 
 # ================= CONFIG =================
-ALIAS1_URL = "https://alias1.onrender.com/ping"
+A1_URL = "https://a1-y11o.onrender.com/ping"
 PING_INTERVAL = 3 * 60  # 3 minutes
 # ==========================================
 
 
 @app.route("/")
 def home():
-    return "Alias2 is running"
+    return "A2 is running"
 
 
 @app.route("/ping")
 def ping():
     print_log("Received ping")
-    return "pong from alias2"
+    return "pong from A2"
 
 
 def print_log(message):
-    print(f"[{datetime.now()}] alias2 | {message}", flush=True)
+    print(f"[{datetime.now()}] A2 | {message}", flush=True)
 
 
-def ping_alias1():
+def ping_A1():
     while True:
         print_log("I am alive")
 
         try:
-            requests.get(ALIAS1_URL, timeout=10)
-            print_log("I called: alias1")
+            requests.get(A1_URL, timeout=10)
+            print_log("I called: A1")
         except Exception as e:
-            print_log(f"alias1 failed: {e}")
+            print_log(f"A1 failed: {e}")
 
         time.sleep(PING_INTERVAL)
 
 
 if __name__ == "__main__":
-    threading.Thread(target=ping_alias1, daemon=True).start()
+    threading.Thread(target=ping_A1, daemon=True).start()
     app.run(host="0.0.0.0", port=10000)
+
